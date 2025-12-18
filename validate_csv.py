9bafad0d49b8aa69c9cc7c623f02c269cecac682
david <david@largitdata.com>
2025-12-18 17:04:23 +0800

    add validate

--- validate_csv.py
+++ validate_csv.py
@@ -5,14 +5,8 @@ import pandas as pd
 failed = False
 
 for file in glob.glob('data/*.csv'):
-    try:
-        df = pd.read_csv(file)
-        a = [c for c in df.columns if 'a' in c]
-        if not a :
-            break 
-        print(f'OK - {len(df)} rows, {len(df.columns)} cols')
-    except Exception as e:
-        print(f'Failed - {e}')
-        failed = True
+    df = pd.read_csv(file)
+    a = [c for c in df.columns if 'a' in c]
+    if not a :
+        assert 1 == 0 
 
-sys.exit(1 if failed else 0)
